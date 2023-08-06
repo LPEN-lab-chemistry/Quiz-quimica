5a609e77b6b0f9741641df1ff32bfe76fa872925
GameGlove <hudsonwerneckferreira@gmail.com>
2023-08-06 20:56:59 -0300

    ground preparation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './pages/Login'
+import './App.css';
+import Login from './pages/Login';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-        <Login></Login>
-
-
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="/conta" />
+          <Route element={<About />} path="/sobre" />
+          <Route element={<Quests />} path="/outras-perguntas" />
+          <Route element={<Quest-eazy />} path="/pergunta-facil" />
+          <Route element={<Quest-medium />} path="/pergunta-media" />
+          <Route element={<Quest-hard />} path="/pergunta-dificil" />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
