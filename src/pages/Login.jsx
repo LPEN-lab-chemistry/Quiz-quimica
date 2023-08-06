8d341417fdd3bc5c3f35de2266667772a3b8807f
Giulianobrjf <giuliano.leite2002@gmail.com>
2023-08-06 00:58:05 -0300

    Criação da tela login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import LoginCadastroForm from '../components/input/LoginCadastroForm';
+import Button from '../components/button/Button';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <header className="header">
+        <div className="left">
+          <h1>Conta</h1>
+        </div>
+        <div className="center">
+          <img src="src\assets/logo.png" alt="Imagem logo mico" />
+        </div>
+        <div className="right">
+          <div className="buttonsobre">
+            <Button>Sobre</Button>
+          </div>
+        </div>
+      </header>
+      <LoginCadastroForm />
+    </div>
+  );
+}
+
+export default Login;
